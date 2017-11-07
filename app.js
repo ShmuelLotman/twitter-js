f437b24bc65a506457deb562194dfd4255ecc3ad
Trey <treycoopermusic@gmail.com>
2017-11-06 19:48:23 -0500

    css

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
 const app = express();
-
-app.use('/', (req, res, next) => {
-    res.send(req.query)
-})
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+app.use('/', routes);
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-   res.send('Welcome to Fake Twitter.')
+    const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
 })
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    if (err) throw new Error;
+});
