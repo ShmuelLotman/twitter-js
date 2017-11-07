ebd20da333ba6e6a30825dd18c9671912fa445f9
Shmuel Lotman <shmuel.lotman@gmail.com>
2017-11-07 14:04:12 -0500

    WE DONE YO

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ const nunjucks = require('nunjucks');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
 const app = express();
+const socketio = require('socket.io');
+const server = app.listen(3000);
+const io = socketio.listen(server);
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure('views'); // point nunjucks to the proper directory for templates
-app.use('/', routes);
+app.use('/', routes(io));
 app.use(express.static('public'))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-
-app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
 var locals = {
     title: 'An Example',
     people: [
--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,35 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+module.exports = function (io) {
 
-
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
- // console.log(tweets);
-
-  res.render( 'index', { tweets: tweets, showForm: true} );
-});
-
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  if(name.match(/^[0-9]+$/) === null) {
-    var list = tweetBank.find( {name: name} );
-  } else {
-    var list = tweetBank.find( {id: parseInt(name)} );
-  }
-  res.render( 'index', { tweets: list, showForm: true, username: name});
-
-});
-
-router.post('/tweets', bodyParser.urlencoded({extended: false}), function(req, res){
-  //console.log('reqbody', req.body);
-  tweetBank.add(req.body.name, req.body.text);
-  res.redirect('/');
-});
-
-module.exports = router;
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+   // console.log(tweets);
+  
+    res.render( 'index', { tweets: tweets, showForm: true} );
+  });
+  
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    if(name.match(/^[0-9]+$/) === null) {
+      var list = tweetBank.find( {name: name} );
+      res.render( 'index', { tweets: list, showForm: true, username: name});
+    } else {
+      var list = tweetBank.find( {id: parseInt(name)} );
+      res.render( 'index', { tweets: list, showForm: true, username: list[0].name});
+    }
+    
+  
+  });
+  
+  router.post('/tweets', bodyParser.urlencoded({extended: false}), function(req, res){
+    //console.log('reqbody', req.body);
+    tweetBank.add(req.body.name, req.body.text);
+    res.redirect('/');
+    var nam = req.body.name,
+    bod = req.body.text;
+    io.sockets.emit('newTweet', {name: nam, tweet: bod});
+  });
+  return router;
+};
