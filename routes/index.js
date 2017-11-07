371d8509e6220f454b5d4f865db25f1084a8ad58
Shmuel Lotman <shmuel.lotman@gmail.com>
2017-11-07 12:18:51 -0500

    we are the champions

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+router.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+router.use(bodyParser.json())
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   console.log(tweets);
 
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true} );
 });
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  if(name.match(/^[0-9]+$/) === null) {
+    var list = tweetBank.find( {name: name} );
+  } else {
+    var list = tweetBank.find( {id: parseInt(name)} );
+  }
+  res.render( 'index', { tweets: list, showForm: true});
+});
+
 
 module.exports = router;
--- tweetBank.js
+++ tweetBank.js
@@ -1,8 +1,9 @@
 const _ = require('lodash');
 var data = [];
-
+var idCount = 0;
 function add (name, content) {
-  data.push({ name: name, content: content });
+  idCount++
+  data.push({ name: name, content: content, id: idCount });
 }
 
 function list () {
@@ -19,18 +20,12 @@ const randArrayEl = function(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-const getFakeName = function() {
-  const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
-  const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
-  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
-};
 
-const getFakeTweet = function() {
-  const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
-  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
-};
+const fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+const fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+const awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive', 'mindblowing', 'amazeballs'];
 
 for (let i = 0; i < 10; i++) {
-  module.exports.add( getFakeName(), getFakeTweet() );
+  module.exports.add((fakeFirsts[i] + ' ' + fakeLasts[i]),  "Fullstack Academy is " + awesome_adj[i] + "! The instructors are just so " + awesome_adj[i] + ". #fullstacklove #codedreams" );
 }
 
